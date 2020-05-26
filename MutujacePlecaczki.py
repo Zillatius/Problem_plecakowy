7d76021903a04f4bb954844748064dff4cc0ee42
Zillatius <mlmaciek4@gmail.com>
2020-05-26 15:41:53 +0200

    wyladnieie

--- MutujacePlecaczki.py
+++ MutujacePlecaczki.py
@@ -9,7 +9,7 @@ class Sack:
         #ustawienie genów na losowe wartości
         for i in range(geneAmt):
             self.geneVals.update({i : random.random()})
-    
+
     def mutate(self):
         # szansa na mutację losowego genu do losowej wartości
         for x in range(len(self.geneVals)):
@@ -43,15 +43,15 @@ class Population:
 
     def popFitness(self,geneWorth,geneSize):
         #liczenie przystosowania i sortowanie
-        global avgFit
-        avgFit = 0;
+        global gAvgFit
+        gAvgFit = 0;
         for spec in self.specimens:
             spec.calcFitness(geneWorth,geneSize)
-            avgFit += spec.fitness
-        avgFit = avgFit/1000
+            gAvgFit += spec.fitness
+        gAvgFit = gAvgFit/gPopulSize
         self.specimens.sort(key=lambda x: x.fitness, reverse=True)
         return self.specimens[0].fitness
-    
+
     def crossover(self):
         #krzyżowanie top 50% populacji
         children = []
@@ -64,7 +64,7 @@ class Population:
         for child in children:
             #wstawienie dzieci do populacji
             self.specimens.append(child)
-        
+
 def LoadData(fileName):
     #ladowanie danych z pliku
     f = open(fileName,"r")
@@ -73,23 +73,25 @@ def LoadData(fileName):
     gWorth = []
     gSize = []
     for x in range(1,len(fLines)):
+        #upewnienie sie czy dane oddzielone spacja czy tabem
         tmp = fLines[x].split(' ')
         if len(tmp) == 1:
             tmp = fLines[x].split('\t')
         gSize.append(float(tmp[1]))
         gWorth.append(float(tmp[2]))
-        
+
     return [gSackSize,gWorth,gSize]
 
 def mateSpecimens(spec1,spec2):
     #wyznaczenie miejsca krzyżowania
-    
     crossPoint = gGenomeSize/2+random.randint(-gGenomeSize/4,gGenomeSize)
+
     #inicjalizacja dzieci
     child1 = Sack(len(spec1.geneVals), spec1.size)
     child2 = Sack(len(spec1.geneVals), spec1.size)
     child1.geneVals={}
     child2.geneVals={}
+
     #wstawienie genów rodziców do dzieci
     for x in range(len(spec1.geneVals)):
         if x < crossPoint:
@@ -98,27 +100,29 @@ def mateSpecimens(spec1,spec2):
         else:
             child1.geneVals.update({x : spec2.geneVals[x]})
             child2.geneVals.update({x : spec1.geneVals[x]})
-    #mutacja dzieci
+
+    #mutacja potomków
     child1.mutate()
     child2.mutate()
     return [child1,child2]
 
+
 #inicjalizacja generatora liczb losowych
 random.seed(a=None)
 global bestCount
-global avgFit
+global gAvgFit
 global gPopulSize
-avgFit = 0
-
+gAvgFit = 0
 
 #ladowanie danych
 params = LoadData("knapsack data large.txt")
 gWorth = params[1]
 gSize = params[2]
 gSackSize = params[0]
-
+#rozmiar populacji
 gPopulSize = 1000
 gGenomeSize = len(gWorth)
+
 #licznik generacji
 genCount = 0
 
@@ -127,35 +131,37 @@ bestCount = 0
 
 #stworzenie populacji
 popul = Population(gPopulSize,gGenomeSize,gSackSize)
+
 #inicjalizacja przystosowania
-bestFit = popul.popFitness(gWorth,gSize)
-newFit = bestFit
+gBestFit = popul.popFitness(gWorth,gSize)
+newFit = gBestFit
+
+#inicjalizacja wartości do wykresu
 plotx = [genCount]
 ploty = []
-ploty.append([bestFit])
-ploty.append([avgFit])
-print(bestFit)
-#tworzenie kolejnych generacji póki wartość nie ustablizuje się na 150 generacji
+ploty.append([gBestFit])
+ploty.append([gAvgFit])
+
+#tworzenie kolejnych generacji póki wartość nie ustablizuje się na 50 generacji
 while bestCount < 50:
     genCount += 1
     popul.crossover()
     newFit = popul.popFitness(gWorth,gSize)
-    if newFit <= bestFit:
+    if newFit <= gBestFit:
         bestCount += 1
     else:
-        bestFit = newFit
-        print(bestFit)
+        gBestFit = newFit
+        print(gBestFit)
         bestCount = 0
     plotx.append(genCount)
-    ploty[0].append(bestFit)
-    ploty[1].append(avgFit)
+    ploty[0].append(gBestFit)
+    ploty[1].append(gAvgFit)
     plt.plot(plotx,ploty[0],'r',plotx,ploty[1],'g')
     plt.pause(0.05)
-    
+
+print("Ilość generacji:")
 print(genCount)
-
-
-
-
-
-
+print("Najlepszy wynik")
+print(gBestFit)
+print("Wciśnij enter by kontynuować...")
+input()
